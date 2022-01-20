3631a1efff2ddce0ddbdcc34fd95d66c62275c98
fasthill <kangeun@naver.com>
2022-01-20 14:21:44 +0900

    update namedtuple

--- python/collections.py
+++ python/collections.py
@@ -1,6 +1,7 @@
 import collections
 
 # namedTuple
+# https://realpython.com/python-namedtuple/
 
 Point = collections.namedtuple('Point', ['x', 'y'])
 p = Point(11, y=22)     # instantiate with positional or keyword arguments
@@ -15,3 +16,14 @@ print(psum)
 print(p)
 
 p.y = 40  # generate error! can't change its value.
+
+Person = collections.namedtuple("Person", "name children")
+john = Person("John Doe", ["Timmy", "Jimmy"]) # the Person -name-d 'John Doe' has two -chidren- 'Timmuy and Jimmy'
+
+print(john)
+
+print(john.children)
+
+john.children.append('Tina')
+# You can create named tuples that contain mutable objects. You can modify the mutable objects in the underlying tuple. 
+print(john.children)
