0f6bf73542a3ce4098fe80208470589290831bcc
fasthill <kangeun@naver.com>
2022-01-25 09:36:37 +0900

    line continuation

---
+++ python/line_continuation.py
@@ -0,0 +1,16 @@
+# 1. use  ( ).  Use parentheses
+# It is preferred to use () rather than \.
+# In Python, you can freely break the line in parentheses ((), {}, []). Using this rule, you can use parentheses instead of backslashes.
+# Since {} is used for set and [] is used for list, use () for such purpose.
+
+a = ( 1 + 2
+     + 3
+     + 10 )
+print(a)
+
+# 2. Use a backslash (\)
+a = 1 + 2 \
++ 3 \
++ 10 
+
+print(a)
