5f6129bd4572f00f4d1f56f62539b62a20622d16
fasthill <kangeun@naver.com>
2023-03-10 12:34:51 +0900

    Update df_merge.py

--- pandas/df_merge.py
+++ pandas/df_merge.py
@@ -5,3 +5,8 @@ df_merged = reduce(lambda  left,right: pd.merge(left,right, how='left', left_ind
 
                                                
 df_base = df_base.merge(df2, how='left', left_index=True, right_index=True)
+df_base = pd.merge(df_base,df_concat, how='outer', left_index=True, right_index=True) # 서로다른 index join
+
+df_concat = pd.concat([df1, df2]) # df1 밑에 df2가 달림 .append와 동일. append는 더이상 사용하지 않음.
+
+
