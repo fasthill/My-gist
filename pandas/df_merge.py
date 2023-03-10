c5cec31c4a4858c7a50e5c1d640dff514a3097b9
fasthill <kangeun@naver.com>
2023-03-10 12:31:41 +0900

    Update df_merge.py

--- pandas/df_merge.py
+++ pandas/df_merge.py
@@ -1,4 +1,7 @@
 from functools import reduce
 
-dfs = [df_estimator, df_concat]
-df_merged= reduce(lambda  left,right: pd.merge(left,right
+dfs = [df_lr_acc, df_sc_acc, df_dt_acc ]
+df_merged = reduce(lambda  left,right: pd.merge(left,right, how='left', left_index=True, right_index=True), dfs)
+
+                                               
+df_base = df_base.merge(df2, how='left', left_index=True, right_index=True)
