3754906062ce17b1b039ce334eabf7f5bab284a8
fasthill <kangeun@naver.com>
2023-09-03 17:34:33 +0900

    Update to_excel.py

--- pandas/to_excel.py
+++ pandas/to_excel.py
@@ -3,4 +3,5 @@ import pandas as pd
 def save(df, filename):
     writer = pd.ExcelWriter('data/'+filename)
     df.to_excel(writer, 'sheet1')
-    writer.save()
+    writer.close()
+    # writer.save() # save()는 더 이상 사용하지 않음. .close()로 대체됨.
