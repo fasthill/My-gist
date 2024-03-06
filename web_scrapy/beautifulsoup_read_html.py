85a91c198e39ed2134ff8c1c3af937689728fab2
fasthill <kangeun@naver.com>
2024-03-06 09:47:35 +0900

    Update beautifulsoup_read_html.py

--- web_scrapy/beautifulsoup_read_html.py
+++ web_scrapy/beautifulsoup_read_html.py
@@ -3,8 +3,10 @@ import requests
 
 import pandas as pd
 
-import cfscrape  # Forbidden 403 발생시 requests 대신 사용
-scraper = cfscrape.create_scraper()
+# import cfscrape  # Forbidden 403 발생시 requests 대신 사용, 에러가 발생하여 아래의 cloudscraper를 사용.
+# scraper = cfscrape.create_scraper() 
+import cloudscraper # Forbidden 403 발생시 requests 대신 사용
+scraper = cloudscraper.create_scraper()
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) \
            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
@@ -17,7 +19,7 @@ html = r.content
 
 class_name = "datatable_table__D_jso datatable_table--border__B_zW0 datatable_table--mobile-basic__W2ilt datatable_table--freeze-column__7YoIE"
 
-table = pd.read_html(html, attrs={"class": class_name},flavor=["lxml", "bs4"])[0]
+table = pd.read_html(io.StringIO(str(html)), attrs={"class": class_name},flavor=["lxml", "bs4"])[0]
 
 table.head()
 
